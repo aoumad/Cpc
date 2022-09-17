8944ee361b70e917d5afd7b489176eac553c2cd6
aoumad <abderazzakoumad@gmail.com>
2022-09-17 07:18:20 +0100

    Create Effictive_Approach.cpp

---
+++ Sheet_program/B/Effictive_Approach.cpp
@@ -0,0 +1,57 @@
+// using Hashmap method
+
+#include <iostream>
+#include <complex>
+#include <iomanip>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <list>
+#include <iterator>
+#include <set>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <climits>
+using namespace std;
+using ll = long long;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<to;x++)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+
+int n, m;
+ll res1 = 0;
+ll res2 = 0;
+int main(int argc, char **argv)
+{
+    string s;int i;
+    if(argc==1) ios::sync_with_stdio(false);
+    FOR(i,argc-1) s+=argv[i+1],s+='\n';
+    FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+    cin >> n;
+    vector<int> arr(n, -1);
+    for (int i = 0;i < n; i++)
+    {
+        int nbr;
+        cin >> nbr;
+        arr[nbr] = i + 1;
+    }
+    cin >> m;
+    while (m--)
+    {
+        int q;
+        cin >> q;
+        res1 += arr[q];
+        res2 += n - arr[q] + 1;
+    }
+    cout << res1 << " " << res2;
+    return (0);
+}
